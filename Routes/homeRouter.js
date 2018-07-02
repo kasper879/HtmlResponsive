962f491f8790040109bd45a786c0b277827e1226
kasper879 <kasper8791@gmail.com>
2018-07-02 11:31:25 +0200

    add html index

--- Routes/homeRouter.js
+++ Routes/homeRouter.js
@@ -2,11 +2,12 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var router = express.Router();
+var path    = require("path");
 
 //var {Product} = require('../Model/product');
 
 module.exports = router;
 
 router.get('/home', (req, res) => {
-    res.sendFile('index.html')
+    res.sendFile(path.join(__dirname+'/index.html'));
 }); 
