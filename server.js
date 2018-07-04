0d5871b5837436fc454a67fe54577e279e863f10
Kasper Nielsen <1054341@ucn.dk>
2018-07-04 21:45:07 +0200

    comot

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(function (req, res, next) {
 
 //setup routes
 app.use(express.static(path.join(__dirname, '/public/'))); 
+app.use('/img',express.static(path.join(__dirname, '/public/img')));
 app.use('/', homeRouter);
 
 
